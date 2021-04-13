be7ffb22238db89dd62646d16407ad444094537c
Bryan Bendall <b_bendall@hotmail.com>
2021-04-12 22:40:15 -0400

    Fix file path

--- premake5.lua
+++ premake5.lua
@@ -16,15 +16,15 @@ workspace "BrytecConfig"
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 IncludeDir = {}
-IncludeDir["GLFW"] = "vendor/GLFW/include"
-IncludeDir["Glad"] = "vendor/Glad/include"
+IncludeDir["GLFW"] = "vendor/glfw/include"
+IncludeDir["Glad"] = "vendor/glad/include"
 IncludeDir["ImGui"] = "vendor/imgui"
 IncludeDir["FontAwesome"] = "vendor/fontawesome"
 
 -- Projects
 group "Dependencies"
-	include "BrytecConfig/vendor/GLFW"
-	include "BrytecConfig/vendor/Glad"
+	include "BrytecConfig/vendor/glfw"
+	include "BrytecConfig/vendor/glad"
 	include "BrytecConfig/vendor/imgui"
 	include "BrytecConfig/vendor/FontAwesome"
 group ""
